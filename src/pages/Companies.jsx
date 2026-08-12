8b3cbdaf4fec901a5e438530ff5c350e11b3c6c2
Santosh Raj <santoshraj20052000@gmail.com>
2026-08-12 17:37:05 +0530

    feat: add company update and delete functionality

--- src/pages/Companies.jsx
+++ src/pages/Companies.jsx
@@ -42,6 +42,31 @@ const Companies = () => {
         })
     }
 
+    const deleteCompany=(idToDelete)=>{
+        const remainingCompanies=companies.filter((company)=>{
+            return company.id!==idToDelete
+        })
+        setCompanies(remainingCompanies);
+    }
+    const [editingCompany, setEditingCompany]=useState(null);
+    const editCompany=(idToEdit)=>{
+        const companyToEdit=companies.find((company)=>{
+            return company.id===idToEdit
+        })
+        setEditingCompany(companyToEdit)
+    }
+
+    const updateCompany = () => {
+    const updatedCompanies = companies.map((company) => {
+        if (company.id === editingCompany.id) {
+            return editingCompany
+        }
+            return company
+        })
+        setCompanies(updatedCompanies)
+        setEditingCompany(null)
+    }
+
   return (
     <>
         <h1>Companies That Are Hiring</h1>
@@ -51,6 +76,8 @@ const Companies = () => {
                 <p>{company.location}</p>
                 <p>Package: {company.package}</p>
                 <p>No. of openings: {company.openings}</p>
+                <button onClick={()=>{deleteCompany(company.id)}}>Delete</button>
+                <button onClick={()=>{editCompany(company.id)}}>Edit</button>
             </div>
         ))}
     <form onSubmit={registerComp}>
@@ -112,6 +139,62 @@ const Companies = () => {
         <br />
         <button type='submit'>Register</button>
     </form>
+
+    {editingCompany &&(
+        <div>
+            <form onSubmit={updateCompany}>
+                <label>Edit Company Name: </label>
+                <input type="text"
+                    placeholder='Edit the companies name'
+                    value={editingCompany.name}
+                    onChange={(e)=>{
+                        setEditingCompany({
+                            ...editingCompany,
+                            name: e.target.value
+                        })
+                    }} 
+                />
+                <br />
+                <label>Edit Opening location: </label>
+                <input type="text"
+                    placeholder='Edit Location'
+                    value={editingCompany.location}
+                    onChange={(e)=>{
+                        setEditingCompany({
+                            ...editingCompany,
+                            location: e.target.value
+                        })
+                    }}
+                />
+                <br />
+                <label>Edit package Details: </label>
+                <input type="number" 
+                    placeholder='Enter package details'
+                    value={editingCompany.package}
+                    onChange={(e)=>{
+                        setEditingCompany({
+                            ...editingCompany,
+                            package: e.target.value
+                        })
+                    }}
+                />
+                <br />
+                <label>Edit No.of Openings: </label>
+                <input type='number'
+                    placeholder='Edit the no. of openings'
+                    value={editingCompany.openings}
+                    onChange={(e)=>{
+                        setEditingCompany({
+                            ...editingCompany,
+                            openings: e.target.value
+                        })
+                    }}
+                />
+                <br />
+                <button type="submit"> Update </button>
+            </form>
+        </div>
+    )}
     </>
   )
 }
